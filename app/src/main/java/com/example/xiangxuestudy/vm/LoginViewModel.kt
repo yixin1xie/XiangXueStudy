133a5ee628cf759a4436c9150f4d54da10d307b4
yixin1.xie <yixin1.xie@tcl.com>
2021-12-30 00:04:23 +0800

    ddd

---
+++ app/src/main/java/com/example/xiangxuestudy/vm/LoginViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.xiangxuestudy.vm
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class LoginViewModel : ViewModel() {
+    fun getLogin(){
+        viewModelScope.launch {
+
+        }
+    }
+}
